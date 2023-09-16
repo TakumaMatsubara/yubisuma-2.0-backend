7b9be77cc2519fd94b78a3c928d396753891f8ce
Yota Takahara <yota.takahara@techouse.jp>
2023-09-16 20:15:39 +0900

    init seed

---
+++ db/init/2_dml.sql
@@ -0,0 +1,123 @@
+USE `yubisuma_api`;
+
+SET NAMES utf8mb4;
+
+INSERT INTO
+    `animals` (
+        `id`,
+        `name`,
+        `numFinger`,
+        `skill`,
+        `handUrl`,
+        `upFingerUrl`,
+        `downFingerUrl`
+    )
+VALUES (
+        UUID(),
+        "ゴリラ",
+        5,
+        "強力な握力",
+        "handUrl_for_ゴリラ",
+        "upFingerUrl_for_ゴリラ",
+        "downFingerUrl_for_ゴリラ"
+    );
+
+INSERT INTO
+    `animals` (
+        `id`,
+        `name`,
+        `numFinger`,
+        `skill`,
+        `handUrl`,
+        `upFingerUrl`,
+        `downFingerUrl`
+    )
+VALUES (
+        UUID(),
+        "いか",
+        10,
+        "インク吹き",
+        "handUrl_for_いか",
+        "upFingerUrl_for_いか",
+        "downFingerUrl_for_いか"
+    );
+
+INSERT INTO
+    `animals` (
+        `id`,
+        `name`,
+        `numFinger`,
+        `skill`,
+        `handUrl`,
+        `upFingerUrl`,
+        `downFingerUrl`
+    )
+VALUES (
+        UUID(),
+        "たこ",
+        8,
+        "吸盤",
+        "handUrl_for_たこ",
+        "upFingerUrl_for_たこ",
+        "downFingerUrl_for_たこ"
+    );
+
+INSERT INTO
+    `animals` (
+        `id`,
+        `name`,
+        `numFinger`,
+        `skill`,
+        `handUrl`,
+        `upFingerUrl`,
+        `downFingerUrl`
+    )
+VALUES (
+        UUID(),
+        "ライオン",
+        5,
+        "強烈な咆哮",
+        "handUrl_for_ライオン",
+        "upFingerUrl_for_ライオン",
+        "downFingerUrl_for_ライオン"
+    );
+
+INSERT INTO
+    `animals` (
+        `id`,
+        `name`,
+        `numFinger`,
+        `skill`,
+        `handUrl`,
+        `upFingerUrl`,
+        `downFingerUrl`
+    )
+VALUES (
+        UUID(),
+        "人",
+        5,
+        "知恵",
+        "handUrl_for_人",
+        "upFingerUrl_for_人",
+        "downFingerUrl_for_人"
+    );
+
+INSERT INTO
+    `animals` (
+        `id`,
+        `name`,
+        `numFinger`,
+        `skill`,
+        `handUrl`,
+        `upFingerUrl`,
+        `downFingerUrl`
+    )
+VALUES (
+        UUID(),
+        "へび",
+        0,
+        "毒",
+        "handUrl_for_へび",
+        "upFingerUrl_for_へび",
+        "downFingerUrl_for_へび"
+    );
